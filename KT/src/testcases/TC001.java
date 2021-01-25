ef3ada1f7a39ee08e06be44f736735dcc953c3d7
Balwan Biradar <Balwan Biradar@DESKTOP-1KFO58I.clarion.local>
2021-01-26 00:14:56 +0530

    Created Auto Project

---
+++ KT/src/testcases/TC001.java
@@ -0,0 +1,26 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC001 {
+	
+	
+	@Test 
+	public void testcase1()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElementById("email").sendKeys("balwan.b");
+		driver.findElementById("pass").sendKeys("123456");
+	driver.quit();
+		
+		
+		
+		
+		
+	}
+	
+
+}
---
+++ KT/src/testcases/TC003.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC003 {
+	
+	
+	@Test 
+	public void testcase1()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElementById("email").sendKeys("balwan.b");
+		driver.findElementById("pass").sendKeys("123456");
+		
+		
+		
+		
+		
+	}
+	
+
+}
